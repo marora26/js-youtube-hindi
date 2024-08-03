cc829a27a100253b53831b561e5a4697f4a57448
marora26 <140274070+marora26@users.noreply.github.com>
2024-08-03 09:54:48 +0000

    again-started

--- 01_basics/datatypes-summary.js
+++
@@ -1,9 +0,0 @@
-// # Primitive Data Types
-
-// 7 types: String, Number, Boolean, Null, undefined, Symbol, BigInt
-
-const score = false
-
-// Refernece (Non primitive)
-
-// Array, Objects, Functions
