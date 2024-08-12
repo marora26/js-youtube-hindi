c3f3d215df2303e2ff4e8096183b6cb68154f58e
marora26 <140274070+marora26@users.noreply.github.com>
2024-08-12 18:08:30 +0000

    CONVERSION-OF-DATA-TYPES

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,73 +1,109 @@
-// let score = "33abc";
-// let valueInNumber = Number(score);
+// // let score = "33abc";
+// // let valueInNumber = Number(score);
 
+
+// // console.log(typeof score);
+// // console.log(typeof valueInNumber);
+// // console.log(score);
+// // console.log(valueInNumber);
+
+
+// // "33" => 33;
+// // "33abc" => Nan;
+// //  true = 1; false = 0;
+
+// // let isLoggedIn = 1;
+// // let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// // console.log(typeof booleanIsLoggedIn);
+// // console.log(booleanIsLoggedIn);
+
+// // 1 = true; 0 = false;
+// // "" => false;
+// // "mayank" => true;
+
+
+// // let someNumber = 33;
+// // let stringNumber = String(someNumber);
+
+// // console.log(stringNumber);
+// // console.log(typeof stringNumber);
+
+// // Operators
+// // Number()
+// // Boolean()
+// // String
+
+// // ********** Operations ************
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+// // console.log(2+2);
+// // console.log(2-2);
+// // console.log(2*2);
+// // console.log(2**3);
+// // console.log(2/3);
+// // console.log(2%3);
+
+// // let str1 = "hello";
+// // let str2 = " mayank";
+
+// // let str3 = str1 + str2;
+
+// // console.log(str3);
+
+// // console.log("1" + 2);
+// // console.log(1 + "2");
+// // console.log("1" + 2 + 2);
+// // console.log(1 + 2 + "2");
+
+// // console.log( (3+4) * 5 % 3 );
+// // console.log(+true);
+// // // console.log(true+);
+// // console.log(+"");
+
+
+
+// // Assignment Operators => prefix operator and postfix operator
+
+// let gameCounter = 100;
+// gameCounter++;
+// console.log(gameCounter);
+
+
+// Revision =>
+
+let score = "33abc"; 
+let valueInNumber = Number(score) // to convert string to number // after conveerting the value is NAn
 
 // console.log(typeof score);
+
 // console.log(typeof valueInNumber);
-// console.log(score);
 // console.log(valueInNumber);
 
+// when we use the new keyword it will give us the datatype as an object
 
-// "33" => 33;
-// "33abc" => Nan;
-//  true = 1; false = 0;
-
-// let isLoggedIn = 1;
-// let booleanIsLoggedIn = Boolean(isLoggedIn);
-
-// console.log(typeof booleanIsLoggedIn);
-// console.log(booleanIsLoggedIn);
-
-// 1 = true; 0 = false;
-// "" => false;
-// "mayank" => true;
+// "33" => 33 Number
+// "33abc" => Nan
+// true => 1 || false => 0
+// null => 0
+// undfined => Nan
 
 
-// let someNumber = 33;
-// let stringNumber = String(someNumber);
+let isLoggedIn = 1;
+let isBooleanLoggedIn = Boolean(isLoggedIn);
 
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
-
-// Operators
-// Number()
-// Boolean()
-// String
-
-// ********** Operations ************
-
-let value = 3;
-let negValue = -value;
-console.log(negValue);
-
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**3);
-// console.log(2/3);
-// console.log(2%3);
-
-// let str1 = "hello";
-// let str2 = " mayank";
-
-// let str3 = str1 + str2;
-
-// console.log(str3);
-
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
-
-// console.log( (3+4) * 5 % 3 );
-// console.log(+true);
-// // console.log(true+);
-// console.log(+"");
+// console.log(isBooleanLoggedIn);
 
 
+// 1 => TRUE 0 => FALSE
+// "" => FALSE 
+// "MAYANK" => TRUE
 
-// Assignment Operators => prefix operator and postfix operator
 
-let gameCounter = 100;
-gameCounter++;
-console.log(gameCounter);
+let someNumber = 33
+let stringNumber = String(someNumber);
+
+console.log(typeof stringNumber);
