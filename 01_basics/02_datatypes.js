5bd3850a9c54c601347e2add56c3cfb697fda6b3
marora26 <140274070+marora26@users.noreply.github.com>
2024-08-12 17:45:01 +0000

    data-types=>1

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,44 +1,75 @@
-"use strict"; // Treat all JS code as newer version
+// "use strict"; // Treat all JS code as newer version
 
-// console.log( 3 + 3 );
-// console.log("Mayank");
+// // console.log( 3 + 3 );
+// // console.log("Mayank");
 
-// alert( 3 + 3 ); // we are using nodejs, not browser 
+// // alert( 3 + 3 ); // we are using nodejs, not browser 
 
-// tc49.ex - Javascript original document Ecmascript
+// // tc49.ex - Javascript original document Ecmascript
 
-// Primary Data Types
+// // Primary Data Types
 
-// number
-// string => ""
-// boolean => true/false
-// bigint
-// null => standalone value
-// undefined => value not assigned yet
-// symbol => unique
-
-
+// // number
+// // string => ""
+// // boolean => true/false
+// // bigint
+// // null => standalone value
+// // undefined => value not assigned yet
+// // symbol => unique
 
 
-// Non-Primary Data Types
+
+
+// // Non-Primary Data Types
+// // object
+// // array
+// // functions
+
+
+// console.log(typeof null); // Typeof Null -> Object
+// console.log(typeof undefined); // Type of undefined => undefined
+
+// const age = 24;
+// const name = "mayank";
+// const check = true;
+// const accno = 1.3931030419421312321312313131212;
+// let x = BigInt(9999999999999999);
+// let y = 9999999999999999;
+
+// console.log(typeof age);
+// console.log(typeof name);
+// console.log(typeof true);
+// console.log(typeof accno);
+// // console.table([typeof x, y]);
+// console.log(typeof x);
+
+// Revison =>
+
+"use strict" // treat all js code as newer version
+
+// alert("Hello World"); // This will work in browser as everythins is inside the document as nodejs don't support
+// we are using node js not browser
+
+// tc39 and mdn for documentation
+
+// Data types
+
+let name = "mayank"; // String
+let age = 18; // Number
+let isLoggedIn = true // Boolean
+let state;
+
+// Primitive Data types
+
+// number => 2 to power 53
+// Bigint => decimal value
+// String => ""
+// Boolean => true/false
+// Null => standalone value || empty 
+// Undefined => when variable is declared but value is not assigned
+// Symbol => unique
+
 // object
-// array
-// functions
 
-
-console.log(typeof null); // Typeof Null -> Object
-console.log(typeof undefined); // Type of undefined => undefined
-
-const age = 24;
-const name = "mayank";
-const check = true;
-const accno = 1.3931030419421312321312313131212;
-let x = BigInt(9999999999999999);
-let y = 9999999999999999;
-
-console.log(typeof age);
-console.log(typeof name);
-console.log(typeof true);
-console.log(typeof accno);
-// console.table([typeof x, y]);
-console.log(typeof x);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
