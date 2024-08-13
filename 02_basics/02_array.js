089673a27db57cd0036472c0a992f0b27c59a246
marora26 <140274070+marora26@users.noreply.github.com>
2024-08-13 14:28:17 +0000

    object-part1

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,17 +1,48 @@
-const marvel_Heros = ["thor", "Ironman", "Spiderman"];
-const dc_Heros = ["Superman", "Flash", "Batman"];
+// const marvel_Heros = ["thor", "Ironman", "Spiderman"];
+// const dc_Heros = ["Superman", "Flash", "Batman"];
 
 
-const new_Heros = marvel_Heros.concat(dc_Heros);
+// const new_Heros = marvel_Heros.concat(dc_Heros);
 
-// console.log(new_Heros);
+// // console.log(new_Heros);
 
-const all_new_heros = [...marvel_Heros, ...dc_Heros];
-console.log(all_new_heros);
-console.log(typeof all_new_heros);
+// const all_new_heros = [...marvel_Heros, ...dc_Heros];
+// console.log(all_new_heros);
+// console.log(typeof all_new_heros);
 
 
 // .flat method to join the array
 
 // Revision =>
 
+const marvel_heros = ["Thor", "Ironman", "Spiderman"];
+const dc_heros = ["superman", "Flash", "batman"];
+
+// const all_heros = marvel_heros.concat(dc_heros);
+// console.log(all_heros);
+
+// push and concat
+// concat will merge all the existing arrays and returns you the new array
+
+// console.log("Hello world");
+
+// spread operator and concat
+
+const new_heros = [...marvel_heros, ...dc_heros]
+
+console.log(new_heros);
+
+const another_array = [1, 2, 3, 4, 5, 6, 7, [6, 7, [4, 5]]];
+
+const real_another_arr = another_array.flat(Infinity); // flat method to retuen all array in one array
+console.log(real_another_arr);
+
+// Array.isArray("Mayank");
+console.log(Array.from("Mayank"));
+
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3)); // to merge multiple variable in one array
--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,44 +1,83 @@
-// 2 ways to declare the objects => 1. Literal 2. Constructor 
-// singleton
+// // 2 ways to declare the objects => 1. Literal 2. Constructor 
+// // singleton
 
-// object literals
-// Object.create => Constructor method through single ton
+// // object literals
+// // Object.create => Constructor method through single ton
 
-// Literal Method =>
+// // Literal Method =>
 
-const mySym = Symbol("key1");
+// const mySym = Symbol("key1");
+
+// const jsUser = {
+//     "name": "Mayank",
+//     "full name": "Mayank Arora",
+//     [mySym]: "mykey1",
+//     "age": 18,
+//     location: "jaipur",
+//     email: "mayank@google.com",
+//     isLoggedIn: false,
+//     lastLoginDays: ["Monday", "Saturday"]
+// } 
+
+// // To acces the value
+// // console.log(jsUser.email);
+// // console.log(jsUser["email", "age"]);
+// // console.log(jsUser[mySym]);
+
+// jsUser.email = "mayank@hotwireglobal.com";
+// // Object.freeze(jsUser);
+// jsUser.email = "mayank@microsoft.com";
+// // console.log(jsUser);
+
+// jsUser.greeting = function(){
+//     console.log("Hello JS user")
+// }
+
+// jsUser.greetingtwo = function(){
+//     console.log(`Hello JS user ${this.name}`);   
+// }
+
+// // To refernce the same object we acan use this
+
+// console.log(jsUser.greeting());
+// console.log(jsUser.greetingtwo());
+
+// Revision =>
+// SingleTon
+
+// constructor method
+// Object.create
+
+
+// Object Literals
+const mysym = Symbol("key1");
 
 const jsUser = {
-    "name": "Mayank",
+    name: "mayank",
     "full name": "Mayank Arora",
-    [mySym]: "mykey1",
-    "age": 18,
+    age: 18,
     location: "jaipur",
-    email: "mayank@google.com",
+    email: "mayank@hotwireglobal.com",
     isLoggedIn: false,
-    lastLoginDays: ["Monday", "Saturday"]
-} 
+    lastLoginDays: ["Monday", "Saturday"],
+    [mysym]: "mykey1"
+}
 
-// To acces the value
-// console.log(jsUser.email);
-// console.log(jsUser["email", "age"]);
-// console.log(jsUser[mySym]);
+// console.log(jsUser[mysym]); // square notation
+// console.log(typeof jsUser[mysym]); // square notation
 
-jsUser.email = "mayank@hotwireglobal.com";
-// Object.freeze(jsUser);
-jsUser.email = "mayank@microsoft.com";
+// jsUser.email = "mayank@google.com"
+// console.log(jsUser["email"]); // square notation
+// Object.freeze(jsUser)
 // console.log(jsUser);
 
-jsUser.greeting = function(){
-    console.log("Hello JS user")
+
+// jsUser.greetings = function(){
+//     console.log("greeting");   
+// }
+
+jsUser.greetings2 = function(){
+    console.log(`Hello js user ${this.email}`);   
 }
-
-jsUser.greetingtwo = function(){
-    console.log(`Hello JS user ${this.name}`);   
-}
-
-// To refernce the same object we acan use this
-
-console.log(jsUser.greeting());
-console.log(jsUser.greetingtwo());
-
+// this  we used to take the same key value
+console.log(jsUser.greetings2());
