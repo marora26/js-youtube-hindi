9e3a2f796898f6d9515a3d035ef37242451fee5a
marora26 <140274070+marora26@users.noreply.github.com>
2024-08-13 08:27:31 +0000

    Math

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,45 +1,77 @@
-// const score = 400;
-// console.log(score);
+// // const score = 400;
+// // console.log(score);
 
 
-const balance = new Number(1000);
-// console.log(balance);
-console.log(balance.toString().length);
-// console.log(balance.toFixed);
-// console.log(typeof balance);
-
-
-
-// const otherNumber = 1123.8966;
-// console.log(otherNumber.toPrecision(3));
-
-// const hundreds = 100000
-// console.log(hundreds.toLocaleString('en-IN'));
+// const balance = new Number(1000);
+// // console.log(balance);
+// console.log(balance.toString().length);
+// // console.log(balance.toFixed);
+// // console.log(typeof balance);
 
 
 
-// ++++++++++++++ Maths +++++++++++++++++
+// // const otherNumber = 1123.8966;
+// // console.log(otherNumber.toPrecision(3));
+
+// // const hundreds = 100000
+// // console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// // ++++++++++++++ Maths +++++++++++++++++
+
+// // console.log(Math);
+
+// // const name = "mayank";
+
+// // console.log(name.length());
 
 // console.log(Math);
 
-// const name = "mayank";
+// console.log(Math.random());
 
-// console.log(name.length());
+// console.log(Math.random()*10);
+// console.log(Math.random()*10 + 1);
 
-console.log(Math);
+// const min = 10
+// const max = 20
 
-console.log(Math.random());
+// // Math.random() * (max -min + 1);
 
-console.log(Math.random()*10);
-console.log(Math.random()*10 + 1);
-
-const min = 10
-const max = 20
-
-// Math.random() * (max -min + 1);
-
-console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
 
 
 // Revision =>
 
+// const score = 400;
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toFixed(1));
+
+// const otherNumber = 123.8966;
+
+// console.log(otherNumber.toPrecision(4));
+
+
+// const hundreds = 100000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// q
+//////// Maths //////////////
+
+// console.log(Math);
+// console.log(Math.abs());
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.3));
+// console.log(Math.min(4, 3, 5, 8));
+// console.log(Math.max(4, 3, 5, 8));
+
+console.log(Math.floor(Math.random()*10+1));
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
