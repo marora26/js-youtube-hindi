1b89b1204273a81fa60891da47331f78e06ca77f
marora26 <140274070+marora26@users.noreply.github.com>
2024-08-13 04:52:03 +0000

    comparison-operators

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -9,9 +9,33 @@
 // console.log("02" > 1);
 // In tyoescript you can compare the variables if the data type is not same.
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
 
 
 // Strict check === to check the value along with the data type
+
+// Revison =>
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);// false
+// console.log(null < 0);// false
+// console.log(null >= 0); // true
+
+// console.log(undefined == 0);
+
+// comparison and equality check == just to check the value === to check the datatype as well
+
+// === strict check valuesa and data type
+
+console.log("1" === 1);
