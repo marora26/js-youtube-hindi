9ed5101bc617c00c03fe6e80c590252f94f05698
marora26 <140274070+marora26@users.noreply.github.com>
2024-08-14 13:14:28 +0000

    arrow

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,40 +1,100 @@
-// let a = 100;
-// const b = 200;
-// var c = 300;
+// // let a = 100;
+// // const b = 200;
+// // var c = 300;
+
+// // if(true){
+    
+// //     let a = 10;
+// //     const b = 20;
+// //     var c = 30;
+// // }
+ 
+// // console.log(a);
+// // console.log(b);
+// // console.log(c);
+
+// // Nested scope
+
+// // function one(){
+// //     const username = "mayank";
+
+// //     function two(){
+// //         const website = "youtube";
+// //         console.log(username);
+        
+// //     }
+
+// //     // console.log(website);
+// //     two()
+// // }
+// // one()
 
 // if(true){
-    
-//     let a = 10;
-//     const b = 20;
-//     var c = 30;
+//     const username = "mayank";
+//     if(username === "mayank"){
+//         const website = "google.com"
+//         console.log(username + website)
+//     }
+//     // console.log(website);
 // }
- 
+// console.log(username);
+
+
+// Revision =>
+
+
+var c = 300; // global scope
+let a = 100
+
+if(true){
+    let a = 10;
+const b = 20;
+// var c = 30;
+// console.log("Inner: ", a);
+
+} // block scope
+
 // console.log(a);
 // console.log(b);
 // console.log(c);
 
-// Nested scope
+// Nessted Scope
 
 // function one(){
-//     const username = "mayank";
+//     const username = "Hitesh"
 
 //     function two(){
-//         const website = "youtube";
+//         const website = "youtube"
 //         console.log(username);
         
 //     }
-
 //     // console.log(website);
-//     two()
+//     two();
+    
 // }
-// one()
+// one();
 
 if(true){
-    const username = "mayank";
-    if(username === "mayank"){
-        const website = "google.com"
-        console.log(username + website)
+    const username = "hitesh";
+    if(username==="hitesh"){
+        const website = "youtube"
+        console.log(username + website);
     }
     // console.log(website);
+    
 }
 // console.log(username);
+
+/// Interesting
+
+addOne(5)
+function addOne(num){
+    return num + 1;
+}
+addTwo()
+
+const addTwo = function(num){
+    return num +2
+}
+
+// function holdind in variable need to be executed after
--- 03_basics/03_Arrow.js
+++ 03_basics/03_Arrow.js
@@ -1,33 +1,36 @@
-// const user = {
-//     username: "Mayank",
-//     price: 999,
-//     welcomeMessage: function(){
-//         console.log(`${this.username} ,welcome to website`);
-//         console.log(this);
-//     } 
-// }
-// user.welcomeMessage();
-// user.username = "Sam";
-// user.welcomeMessage();
-// // this is for current context to acceess the variables
+// // const user = {
+// //     username: "Mayank",
+// //     price: 999,
+// //     welcomeMessage: function(){
+// //         console.log(`${this.username} ,welcome to website`);
+// //         console.log(this);
+// //     } 
+// // }
+// // user.welcomeMessage();
+// // user.username = "Sam";
+// // user.welcomeMessage();
+// // // this is for current context to acceess the variables
 
-// function chai(){
-//     let username = "mayank";
-//     console.log(this.username);    
-// }
-// chai();
+// // function chai(){
+// //     let username = "mayank";
+// //     console.log(this.username);    
+// // }
+// // chai();
 
-// const chai = () => {
-//     let username = "mayank";
-//     console.log(this.username);    
-// }
+// // const chai = () => {
+// //     let username = "mayank";
+// //     console.log(this.username);    
+// // }
 
-// chai();
+// // chai();
 
-const addTwo = (num1, num2) =>  (num1 + num2);
+// const addTwo = (num1, num2) =>  (num1 + num2);
 
-// Implicit return
-// {} => then retun required
-// () => then return not required
+// // Implicit return
+// // {} => then retun required
+// // () => then return not required
 
-console.log(addTwo(2,4));
+// console.log(addTwo(2,4));
+
+// Revision =>
+
